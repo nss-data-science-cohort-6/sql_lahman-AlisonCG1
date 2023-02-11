07843794b469057370c76a17ca39407d42049c73
AlisonCG1 <alisoncg@outlook.com>
2023-02-11 15:11:56 -0500

    question 6

--- lahman.sql
+++ lahman.sql
@@ -56,55 +56,131 @@
 
 --3.Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends? (Hint: For this question, you might find it helpful to look at the generate_series function 
 
---WITH decades AS (
---	SELECT * FROM generate_series(1870, 2021, 10)AS gen)
---SELECT gen AS Decade, ROUND(AVG((so / g)), 2) AS avg_strikeouts_per_game
---FROM pitching p
---INNER JOIN decades
---ON p.yearid BETWEEN gen AND gen+9
---GROUP BY Decade;
+-- WITH decades AS (
+-- 	SELECT * FROM generate_series(1870, 2021, 10)AS gen)
+-- SELECT gen AS Decade, ROUND(AVG((so / g)), 2) AS avg_strikeouts_per_game
+-- FROM pitching p
+-- INNER JOIN decades
+-- ON p.yearid BETWEEN gen AND gen+9
+-- GROUP BY Decade;
+
+--WITH decade_cte AS (
+--	SELECT generate_series(1920, 2020, 10) AS beginning_of_decade
+--)
+--SELECT 
+--	ROUND(SUM(hr) * 1.0 / (SUM(g) / 2), 2) AS hr_per_game,
+--	ROUND(SUM(so) * 1.0 / (SUM(g) / 2), 2) AS so_per_game,
+--	beginning_of_decade::text || 's' AS decade
+--FROM teams
+--INNER JOIN decade_cte
+--ON yearid BETWEEN beginning_of_decade AND beginning_of_decade + 9
+--WHERE yearid >= 1920
+--GROUP BY decade
+--ORDER BY decade;
 
 --4.Find the player who had the most success stealing bases in 2016, where success is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted at least 20 stolen bases. Report the players' names, number of stolen bases, number of attempts, and stolen base percentage.
---SELECT success.nameFirst, success.nameLast, CAST(CAST(Success.stolen_bases AS DECIMAL(5, 2)) / success.total_attempts * 100 AS DECIMAL(5, 2)) AS success_stealing 
---FROM(SELECT nameFirst, nameLast, SUM(sb) AS stolen_bases, SUM(sb + cs) AS total_attempts
---	FROM people
---	INNER JOIN batting AS B
---	USING(playerid)
---	WHERE sb >= 20 
---		AND yearid = 2016
---	GROUP BY nameFirst, nameLast) AS success
---ORDER BY success_stealing DESC
---LIMIT 1;
+SELECT success.nameFirst, success.nameLast, CAST(CAST(Success.stolen_bases AS DECIMAL(5, 2)) / success.total_attempts * 100 AS DECIMAL(5, 2)) AS success_stealing 
+FROM(SELECT nameFirst, nameLast, SUM(sb) AS stolen_bases, SUM(sb + cs) AS total_attempts
+	FROM people
+	INNER JOIN batting AS B
+	USING(playerid)
+	WHERE sb >= 20 
+		AND yearid = 2016
+	GROUP BY nameFirst, nameLast) AS success
+ORDER BY success_stealing DESC;
 
 --5. From 1970 to 2016, what is the largest number of wins for a team that did not win the world series? 
 --What is the smallest number of wins for a team that did win the world series? 
 --Doing this will probably result in an unusually small number of wins for a world series champion; 
---determine why this is the case. Then redo your query, excluding the problem year. 
---How often from 1970 to 2016 was it the case that a team with the most wins also won the world series? 
---What percentage of the time?
 
---(SELECT *
---FROM (SELECT franchid AS franchise,
---	SUM(CASE WHEN WSWin = 'Y' THEN 1 ELSE 0 END) AS total_ws_wins,
---	SUM(W) AS total_regular_wins
+--SELECT *
+--FROM (SELECT franchid AS franchise, yearid AS year,
+--	CASE WHEN WSWin = 'Y' THEN 1 ELSE 0 END AS total_ws_wins,
+--	W AS total_regular_wins
 --FROM teams
 --WHERE yearid BETWEEN 1970 AND 2016
---GROUP BY franchid
---ORDER BY franchid) AS winners
+--ORDER BY franchid) AS franch
 --WHERE total_ws_wins = 0
 --ORDER BY total_regular_wins DESC
---LIMIT 1
---)
---UNION ALL
---(SELECT *
---FROM (SELECT franchid AS franchise,
---	SUM(CASE WHEN WSWin = 'Y' THEN 1 ELSE 0 END) AS total_ws_wins,
---	SUM(W) AS total_regular_wins
---FROM teams
+--LIMIT 1;
+
+--SELECT *
+--FROM (SELECT yearid AS year,
+--	  franchid AS franchise,
+--	CASE WHEN WSWin = 'Y' THEN 1 ELSE 0 END AS total_ws_wins,
+--	W AS total_regular_wins
+--FROM teams AS t
 --WHERE yearid BETWEEN 1970 AND 2016
---GROUP BY franchid
---ORDER BY franchid) AS losses
+--ORDER BY yearid) AS franchise
 --WHERE total_ws_wins <> 0
---ORDER BY total_regular_wins
---LIMIT 1
---);
+--ORDER BY total_ws_wins
+--LIMIT 1;
+
+
+--Then redo your query, excluding the problem year. 
+--How often from 1970 to 2016 was it the case that a team with the most wins also won the world series? 
+--What percentage of the time?
+--WITH wins_by_year AS (SELECT yearid, franchid, w AS wins, wswin as world_series_win,
+--							RANK() OVER(PARTITION BY yearid ORDER BY w DESC) AS Rank_by_Total_Wins
+--						FROM teams
+--						WHERE yearid BETWEEN 1970 AND 2016
+--						AND yearid <> 1981
+--						ORDER BY yearid)
+--SELECT yearid, franchid, world_series_win
+--FROM wins_by_year
+--WHERE wins_by_year.Rank_by_Total_Wins = 1 AND world_series_win = 'Y'
+--ORDER BY yearid
+ 
+
+--6.Which managers have won the TSN Manager of the Year award in both the National League (NL) 
+--and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+
+SELECT * 
+FROM people
+INNER JOIN Managers 
+USING(playerid)
+INNER JOIN AwardsManagers 
+USING(playerid)
+WHERE awardid = 'TSN Manager of the Year'
+
+
+WITH manager AS(
+	SELECT namefirst AS name, namelast AS lastname, 
+		   awardid, yearid, lgid
+	FROM people AS plp
+	INNER JOIN AwardsManagers AS mgr
+	USING(playerid)
+	WHERE awardid = 'TSN Manager of the Year'),
+Awards AS(
+SELECT namefirst AS name, namelast AS lastname, 
+		   awardid, yearid, lgid
+	FROM people AS plp
+	INNER JOIN AwardsManagers AS mgr
+	USING(playerid)
+	WHERE lgid = 'AL' AND 'NL')
+	
+SELECT name, lastname, 
+	    awardid, yearid, lgid,
+		name, lastname, 
+	   awardid, yearid, lgid
+FROM manager
+INNER JOIN Awards
+USING(playerid)
+
+SELECT TSN_WINNERS_Names.playerid, TSN_WINNERS_Names.nameFirst, TSN_WINNERS_Names.nameLast,TSN_WINNERS_Names.Team2, COUNT( DISTINCT lgid) AS lg_count
+		FROM (WITH TSN_WINNERS AS (SELECT awardsmanagers.yearid, awardsmanagers.playerid, awardsmanagers.lgid, managers.teamid AS Team, awardid
+				FROM awardsmanagers
+				INNER JOIN managers
+				ON awardsmanagers.playerid = managers.playerid AND awardsmanagers.yearid = managers.yearid
+				WHERE awardid LIKE '%TSN%')
+			SELECT TSN_WINNERS.yearid, TSN_WINNERS.playerid, nameFirst, nameLast, TSN_WINNERS.Team AS Team2, TSN_WINNERS.lgid
+			FROM people
+			INNER JOIN TSN_WINNERS
+			USING (playerid)) AS TSN_WINNERS_Names
+		WHERE TSN_WINNERS_Names.playerid IN ('leylaji99', 'johnsda02', 'coxbo01', 'larusto01')
+		GROUP BY TSN_WINNERS_Names.playerid, TSN_WINNERS_Names.nameFirst, TSN_WINNERS_Names.nameLast, TSN_WINNERS_Names.Team2
+		ORDER BY lg_count DESC
+
+
+select *
+FROM AwardsManagers 
